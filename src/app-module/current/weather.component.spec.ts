ca3b4f9844cbc8bb8a8d352ef2d2dcd607a37d45
Milan <milan.saxena3@t-mobile.com>
2018-03-05 22:06:35 +0530

    update test cases

--- src/app-module/current/weather.component.spec.ts
+++ src/app-module/current/weather.component.spec.ts
@@ -1,34 +1,44 @@
-import { TestBed, async } from '@angular/core/testing';
-import { MatToolbarModule,
-  MatButtonModule,
-    MatCardModule,
-    MatDividerModule,
-    MatInputModule,
-    MatIconModule,
-    MatMenuModule,
-    MatRadioModule,
-    MatSelectModule } from '@angular/material';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { TestBed, async, inject } from '@angular/core/testing';
+import {
+  MatToolbarModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatDatepickerModule,
+  MatDividerModule,
+  MatGridListModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatRadioModule,
+  MatSelectModule
+} from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FusionChartsModule } from 'angular4-fusioncharts';
-import { CurrentWeatherComponent } from './weather.component';
-import { CurrentWeatherService } from './weather.service';
-import { HttpModule }    from '@angular/http';
+import { CurrentWeatherComponent } from '../current/weather.component';
+import { CurrentWeatherService } from '../current/weather.service';
+import { HttpModule } from '@angular/http';
 
-describe('WeatherComponent', () => {
+describe('ForecastWeatherComponent', () => {
+  const weatherCurrentDataMock = require('../../assets/mock/mock-weather-current-data.json');
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [CurrentWeatherComponent],
       providers: [CurrentWeatherService],
       imports: [
-        MatButtonModule,
+        MatToolbarModule,
+        MatButtonToggleModule,
         MatCardModule,
+        MatCheckboxModule,
+        MatDatepickerModule,
         MatDividerModule,
+        MatGridListModule,
         MatInputModule,
-        MatIconModule,
+        MatListModule,
         MatMenuModule,
         MatRadioModule,
         MatSelectModule,
-        MatToolbarModule,
         FormsModule,
         HttpModule,
         FusionChartsModule
@@ -40,10 +50,65 @@ describe('WeatherComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'Weather Man'`, async(() => {
+
+  it('should create the app', async(() => {
     const fixture = TestBed.createComponent(CurrentWeatherComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Weather Man');
+    const type = 'CityName';
+    fixture.debugElement.componentInstance.onChangeType(type);
+    expect(fixture.debugElement.componentInstance.isCityNameSelected).toEqual(true);
+    expect(fixture.debugElement.componentInstance.isZipCodeSelected).toEqual(false);
   }));
-  
+
+  it('should call onChangeType method', async(() => {
+    const fixture = TestBed.createComponent(CurrentWeatherComponent);
+    const type = 'ZIPCode';
+    fixture.debugElement.componentInstance.onChangeType(type);
+    expect(fixture.debugElement.componentInstance.isCityNameSelected).toEqual(false);
+    expect(fixture.debugElement.componentInstance.isZipCodeSelected).toEqual(true);
+  }));
+
+  it('should invoke getCurrentWeather when selectedType is CityName',
+    async(inject([CurrentWeatherService], (currentWeatherService) => {
+      const fixture = TestBed.createComponent(CurrentWeatherComponent);
+      fixture.debugElement.componentInstance.selectedType = 'CityName';
+      spyOn(currentWeatherService, 'getCurrentWeather').and.callFake(function () {
+        return {
+          then: function (callback) { return callback(weatherCurrentDataMock); }
+        };
+      });
+
+      fixture.componentInstance.getCurrentWeather();
+      expect(currentWeatherService.getCurrentWeather).toHaveBeenCalled();
+    }))
+  );
+
+  it('should invoke getCurrentWeather when selectedType is CityID',
+  async(inject([CurrentWeatherService], (currentWeatherService) => {
+    const fixture = TestBed.createComponent(CurrentWeatherComponent);
+    fixture.debugElement.componentInstance.selectedType = 'CityID';
+    spyOn(currentWeatherService, 'getCurrentWeather').and.callFake(function () {
+      return {
+        then: function (callback) { return callback(weatherCurrentDataMock); }
+      };
+    });
+
+    fixture.componentInstance.getCurrentWeather();
+    expect(currentWeatherService.getCurrentWeather).toHaveBeenCalled();
+  }))
+);
+
+it('should invoke getCurrentWeather when selectedType is ZIPCode',
+async(inject([CurrentWeatherService], (currentWeatherService) => {
+  const fixture = TestBed.createComponent(CurrentWeatherComponent);
+  fixture.debugElement.componentInstance.selectedType = 'ZIPCode';
+  spyOn(currentWeatherService, 'getCurrentWeather').and.callFake(function () {
+    return {
+      then: function (callback) { return callback(weatherCurrentDataMock); }
+    };
+  });
+
+  fixture.componentInstance.getCurrentWeather();
+  expect(currentWeatherService.getCurrentWeather).toHaveBeenCalled();
+}))
+);
 });
--- src/app-module/forecast/weather.component.spec.ts
+++ src/app-module/forecast/weather.component.spec.ts
@@ -1,34 +1,44 @@
-import { TestBed, async } from '@angular/core/testing';
-import { MatToolbarModule,
-  MatButtonModule,
-    MatCardModule,
-    MatDividerModule,
-    MatInputModule,
-    MatIconModule,
-    MatMenuModule,
-    MatRadioModule,
-    MatSelectModule } from '@angular/material';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { TestBed, async, inject } from '@angular/core/testing';
+import {
+  MatToolbarModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatDatepickerModule,
+  MatDividerModule,
+  MatGridListModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatRadioModule,
+  MatSelectModule
+} from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FusionChartsModule } from 'angular4-fusioncharts';
-import { ForecastWeatherComponent } from './weather.component';
-import { ForecastWeatherService } from './weather.service';
-import { HttpModule }    from '@angular/http';
+import { ForecastWeatherComponent } from '../forecast/weather.component';
+import { ForecastWeatherService } from '../forecast/weather.service';
+import { HttpModule } from '@angular/http';
 
-describe('WeatherComponent', () => {
+describe('ForecastWeatherComponent', () => {
+  const weatherForecastDataMock = require('../../assets/mock/mock-weather-forecast-data.json');
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ForecastWeatherComponent],
       providers: [ForecastWeatherService],
       imports: [
-        MatButtonModule,
+        MatToolbarModule,
+        MatButtonToggleModule,
         MatCardModule,
+        MatCheckboxModule,
+        MatDatepickerModule,
         MatDividerModule,
+        MatGridListModule,
         MatInputModule,
-        MatIconModule,
+        MatListModule,
         MatMenuModule,
         MatRadioModule,
         MatSelectModule,
-        MatToolbarModule,
         FormsModule,
         HttpModule,
         FusionChartsModule
@@ -40,10 +50,35 @@ describe('WeatherComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'Weather Man'`, async(() => {
+
+  it('should create the app', async(() => {
     const fixture = TestBed.createComponent(ForecastWeatherComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Weather Man');
+    const type = 'CityName';
+    fixture.debugElement.componentInstance.onChangeType(type);
+    expect(fixture.debugElement.componentInstance.isCityNameSelected).toEqual(true);
+    expect(fixture.debugElement.componentInstance.isZipCodeSelected).toEqual(false);
   }));
-  
+
+  it('should call onChangeType method', async(() => {
+    const fixture = TestBed.createComponent(ForecastWeatherComponent);
+    const type = 'ZIPCode';
+    fixture.debugElement.componentInstance.onChangeType(type);
+    expect(fixture.debugElement.componentInstance.isCityNameSelected).toEqual(false);
+    expect(fixture.debugElement.componentInstance.isZipCodeSelected).toEqual(true);
+  }));
+
+  it('should invoke getWeatherForecast',
+    async(inject([ForecastWeatherService], (forecastWeatherService) => {
+      const fixture = TestBed.createComponent(ForecastWeatherComponent);
+
+      spyOn(forecastWeatherService, 'getWeatherForecast').and.callFake(function () {
+        return {
+          then: function (callback) { return callback(weatherForecastDataMock); }
+        };
+      });
+
+      fixture.componentInstance.getWeatherForecast();
+      expect(forecastWeatherService.getWeatherForecast).toHaveBeenCalled();
+    }))
+  );
 });
