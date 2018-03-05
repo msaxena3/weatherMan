cfdbb4de267074ae8e482a4e6e793416977ddab8
Milan <milan.saxena3@t-mobile.com>
2018-03-05 17:29:40 +0530

    update component

--- src/app-module/current/weather.component.spec.ts
+++ src/app-module/current/weather.component.spec.ts
@@ -1,40 +1,34 @@
 import { TestBed, async } from '@angular/core/testing';
 import { MatToolbarModule,
-  MatButtonToggleModule,
-  MatCardModule,
-  MatCheckboxModule,
-  MatDatepickerModule,
-  MatDividerModule,
-  MatGridListModule,
-  MatInputModule,
-  MatListModule,
-  MatMenuModule,
-  MatRadioModule,
-  MatSelectModule } from '@angular/material';
+  MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatInputModule,
+    MatIconModule,
+    MatMenuModule,
+    MatRadioModule,
+    MatSelectModule } from '@angular/material';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { FusionChartsModule } from 'angular4-fusioncharts';
-import { WeatherComponent } from './weather.component';
-import { WeatherService } from './weather.service';
+import { CurrentWeatherComponent } from './weather.component';
+import { CurrentWeatherService } from './weather.service';
 import { HttpModule }    from '@angular/http';
 
 describe('WeatherComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [WeatherComponent],
-      providers: [WeatherService],
+      declarations: [CurrentWeatherComponent],
+      providers: [CurrentWeatherService],
       imports: [
-        MatToolbarModule,
-        MatButtonToggleModule,
+        MatButtonModule,
         MatCardModule,
-        MatCheckboxModule,
-        MatDatepickerModule,
         MatDividerModule,
-        MatGridListModule,
         MatInputModule,
-        MatListModule,
+        MatIconModule,
         MatMenuModule,
         MatRadioModule,
         MatSelectModule,
+        MatToolbarModule,
         FormsModule,
         HttpModule,
         FusionChartsModule
@@ -42,14 +36,14 @@ describe('WeatherComponent', () => {
     }).compileComponents();
   }));
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(WeatherComponent);
+    const fixture = TestBed.createComponent(CurrentWeatherComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  // it(`should have as title 'Weather Man'`, async(() => {
-  //   const fixture = TestBed.createComponent(WeatherComponent);
-  //   const app = fixture.debugElement.componentInstance;
-  //   expect(app.title).toEqual('Weather Man');
-  // }));
+  it(`should have as title 'Weather Man'`, async(() => {
+    const fixture = TestBed.createComponent(CurrentWeatherComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('Weather Man');
+  }));
   
 });
